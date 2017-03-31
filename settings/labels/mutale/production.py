af5ee45141f6b666a44e7fb0e53141da5f67fa76
P. Mutale <pmutale@vacansoleil.com>
2017-03-31 12:22:39 +0200

    Allowed Hosts

--- settings/labels/mutale/production.py
+++ settings/labels/mutale/production.py
@@ -1,5 +1,11 @@
 from settings.labels.mutale.base import *
+import dj_database_url
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['mutale.herokuapp.com']
+
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+
+DATABASES['default'].update(db_from_env)
