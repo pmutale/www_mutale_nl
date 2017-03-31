e6404b7349492b5a8bb21f288fc6f40b822bfdf0
P. Mutale <pmutale@vacansoleil.com>
2017-03-31 11:34:54 +0200

    ProcFile

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -15,6 +15,6 @@ from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-application = django.core.handlers.wsgi.WSGIHandler()
-# application = get_wsgi_application
+# application = django.core.handlers.wsgi.WSGIHandler()
+application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
