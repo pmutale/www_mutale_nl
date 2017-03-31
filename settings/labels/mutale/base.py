df53253988a76784676693dca843567f9c37f2b6
P. Mutale <pmutale@vacansoleil.com>
2017-03-31 10:45:07 +0200

    Settings

--- settings/labels/mutale/base.py
+++ settings/labels/mutale/base.py
@@ -154,3 +154,5 @@ THUMBNAIL_PROCESSORS = (
     'filer.thumbnail_processors.scale_and_crop_with_subject_location',
     'easy_thumbnails.processors.filters'
 )
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
