95404c40921f792e934f956b44071e75412175a9
Peter Mutale <peter@mutale.nl>
2017-09-07 17:14:18 +0200

    "Stick2Uganda"

---
+++ registration/urls.py
@@ -0,0 +1,15 @@
+# from django.conf.urls import url
+# from django.contrib.auth import views as auth_views
+#
+# from . import views
+#
+# __all__ = ('urlpatterns',)
+#
+# app_name = 'registraion'
+# urlpatterns = [
+#     url(
+#         '^login/$',
+#         auth_views.login,
+#         {'template_name': 'registration/login.html'}
+#     ),
+# ]
