24f0a89e9053e9e25d567e4ee79a0f6784614b78
Peter Mutale <peter@mutale.nl>
2017-09-09 17:08:07 +0200

    "FontFamilyChange"

--- Procfile
+++ Procfile
@@ -1,1 +1,5 @@
+release: cd themes && npm install gulp
+release: cd themes && npm install
+release: cd themes && gulp production
+release: python manage.py collectstatic -v0 --noinput
 web: gunicorn mysite.wsgi --log-file -
--- themes/gulpfile.js
+++ themes/gulpfile.js
@@ -19,10 +19,11 @@ gulp.task('bootstrap-js', function() {
     return gulp.src(
         [
             'node_modules/bootstrap/dist/js/bootstrap.min.js',
+            'node_modules/jquery/dist/jquery.slim.min.js',
             'node_modules/jquery/dist/jquery.min.js',
             'node_modules/tether/dist/js/tether.min.js',
-            'node_modules/jquery-countdown/dist/jquery.countdown.js',
-            'node_modules/jquery-countdown/src/countdown.js'
+            'node_modules/popper.js/dist/umd/popper.min.js',
+            'node_modules/jquery-countdown/dist/jquery.countdown.js'
         ])
         .pipe(gulp.dest(dest + "/js/bootsrap"))
         .pipe(browserSync.stream());
@@ -32,11 +33,19 @@ gulp.task('bootstrap-js', function() {
 gulp.task('serve', ['bootstrap-css'], function() {
 
     browserSync.init({
-        server: './' + dest
+        server: './' + dest + '/scss'
     });
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch(
+        [
+            'node_modules/bootstrap/scss/bootstrap.scss',
+            './static/themes/scss/*.scss',
+            './static/themes/default/*.scss'
+        ], [
+            'bootstrap-css'
+        ]);
+    gulp.watch("./templates/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('default', ['bootstrap-js','serve']);
+gulp.task('production', ['bootstrap-js','bootstrap-css']);
