3411a4793139e575f8a8fa2218605452cfe13b32
Peter Mutale <peter@mutale.nl>
2017-09-12 16:55:55 +0200

    "master-a"

--- settings/labels/mutale/acceptance.py
+++ settings/labels/mutale/acceptance.py
@@ -6,7 +6,7 @@ ALLOWED_HOSTS = ['mutale-acc.herokuapp.com', '127.0.0.1', 'localhost']
 
 DATABASE_URL = 'postgres://tchjvyogxumkol:101b8068787cea9f1eb05f0f20abeefe3cf4b5797ccd12f96d5abb8ab3d9d1cb@ec2-79-125' \
                '-26-23.eu-west-1.compute.amazonaws.com:5432/d3dpa9mm8f5aut'
-
+DEBUG = True
 
 DATABASES = {
     'default':
