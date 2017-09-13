ac3c8274b3b1e805fbb4f06f88215ab341037837
Peter Mutale <peter@mutale.nl>
2017-09-13 14:12:09 +0200

    "master"

--- settings/labels/mutale/acceptance.py
+++ settings/labels/mutale/acceptance.py
@@ -5,7 +5,10 @@ from themes import secrets
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['mutale.herokuapp.com', '127.0.0.1', 'localhost', 'mutale-acc.herokuapp.com', 'mutale-dev.herokuapp.com']
+ALLOWED_HOSTS = ['mutale.herokuapp.com',
+                 '127.0.0.1', 'localhost',
+                 'mutale-acc.herokuapp.com',
+                 'mutale-dev.herokuapp.com']
 
 DATABASE_URL = 'postgres://oogcsuzgfwhqbc:0da4b0d51b2f508e4c00308e3c583c2dd9999b6b439a5501dcd643602b455167@ec2-54' \
                '-247-92-185.eu-west-1.compute.amazonaws.com:5432/dmtkic08buj90'
