78be8ed641b5827f93895fe6a18bc03986e08488
Peter Mutale <peter@mutale.nl>
2017-09-21 10:55:25 +0200

    "master"

--- mysite/context_processors.py
+++ mysite/context_processors.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.http.response import HttpResponse
 
 
 def google_analytics(request):
@@ -10,15 +11,18 @@ def google_analytics(request):
     ga_prop_id = getattr(settings, 'GOOGLE_ANALYTICS_PROPERTY_ID', False)
     ga_domain = getattr(settings, 'GOOGLE_ANALYTICS_DOMAIN', False)
 
-    state = True
+    response = HttpResponse()
 
-    if not settings.DEBUG and ga_prop_id and ga_domain:
-        return {
-            'GOOGLE_ANALYTICS_PROPERTY_ID': ga_prop_id if state else '',
-            'GOOGLE_ANALYTICS_DOMAIN': ga_domain if state else ''
-        }
-    elif settings.DEBUG:
-        return {
-            'GOOGLE_ANALYTICS_PROPERTY_ID': 'development',
-            'GOOGLE_ANALYTICS_DOMAIN': 'auto'
-        }
+    if response.status_code in [500, 400]:
+        stop = True
+
+        if not settings.DEBUG and ga_prop_id and stop:
+            return {
+                'GOOGLE_ANALYTICS_PROPERTY_ID': ga_prop_id,
+                'GOOGLE_ANALYTICS_DOMAIN': ga_domain
+            }
+        elif settings.DEBUG:
+            return {
+                'GOOGLE_ANALYTICS_PROPERTY_ID': 'development',
+                'GOOGLE_ANALYTICS_DOMAIN': 'auto'
+            }
