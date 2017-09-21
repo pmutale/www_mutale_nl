25864b7b26c1a0084cef155f608baa588281d7e8
Peter Mutale <peter@mutale.nl>
2017-09-21 10:25:34 +0200

    "master"

--- mysite/context_processors.py
+++ mysite/context_processors.py
@@ -10,10 +10,12 @@ def google_analytics(request):
     ga_prop_id = getattr(settings, 'GOOGLE_ANALYTICS_PROPERTY_ID', False)
     ga_domain = getattr(settings, 'GOOGLE_ANALYTICS_DOMAIN', False)
 
+    state = True
+
     if not settings.DEBUG and ga_prop_id and ga_domain:
         return {
-            'GOOGLE_ANALYTICS_PROPERTY_ID': ga_prop_id,
-            'GOOGLE_ANALYTICS_DOMAIN': ga_domain
+            'GOOGLE_ANALYTICS_PROPERTY_ID': ga_prop_id if state else '',
+            'GOOGLE_ANALYTICS_DOMAIN': ga_domain if state else ''
         }
     elif settings.DEBUG:
         return {
