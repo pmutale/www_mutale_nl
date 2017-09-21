eb246b92728ace2f33929b9c025b81808f1954c6
Peter Mutale <peter@mutale.nl>
2017-09-21 16:12:24 +0200

    "MemCache-Config"

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -18,3 +18,6 @@ from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+
+from django.core.cache.backends.memcached import BaseMemcachedCache
+BaseMemcachedCache.close = lambda self, **kwargs: None
